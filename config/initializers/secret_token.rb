99b56815fa310a1170e067402c9d8c5a4f9ef81a
unknown <gur32465@GURWIFTH1D41.ad.aricent.com>
2013-09-19 12:04:36 +0530

    hello

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '21537d4784d1ee52440c0a77d217f162cfad9e047b70f79c9eeabb72a095a13eb75312d23e42e3de3f7c3d3dc0694dc8b1c7da2c2e8d703e68b2f54d66fb5279'
